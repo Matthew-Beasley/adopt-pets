cba0dee001f9fa8f0675bc9d65ec7246ecdd6cba
conbec <56614504+conbec@users.noreply.github.com>
2020-01-04 17:03:40 -0800

    getting data, promise

--- js/app.js
+++ js/app.js
@@ -1,8 +1,95 @@
 const submit = document.querySelector('#submit-btn');
+const zip = document.querySelector('#location')
+const species = document.querySelector('#species');
+const breed = document.querySelector('#breed');
+const male = document.querySelector('#male');
+const female = document.querySelector('#female');
+const both = document.querySelector('#both');
+const size = document.querySelector('#size');
+const age = document.querySelector('#age');
+const rightPanel = document.querySelector('#right-panel');
 
+const baseUrl = 'https://api.rescuegroups.org/http/v2.json';
 
 //////// initial search  //////////
-// catch submit event
+const displaySearch = (results) => {
+    const pets = results.data;
+    console.log(pets)
+    let html = '';
+    for (let pet in pets) {
+        if (pet){
+            console.log(pet);
+            html += `<h3>${pet.animalName}</h3>`
+        }
+    }
+    rightPanel.innerHTML = html;
+
+}
+
+const createSearchObject = () => {
+
+    const search = {
+        apikey: '6QONihuq',
+        objectType: 'animals',
+        objectAction: 'publicSearch',
+        search:
+        {
+            resultStart: '0',
+            resultLimit: '10',
+            resultSort: 'animalID',
+            resultOrder: 'asc',
+            filters:
+                [
+                    {
+                        fieldName: 'animalStatus',
+                        operation: 'equals',
+                        criteria: 'Available'
+                    },
+                    {
+                        fieldName: 'animalLocationDistance',
+                        operation: 'radius',
+                        criteria: '50'
+                    },
+                    {
+                        fieldName: 'animalLocation',
+                        operation: 'equals',
+                        criteria: `${zip.value}`
+                    }
+                ],
+            filterProcessing: '1',
+            fields:
+                [
+                    'animalID', 'animalOrgID', 'animalActivityLevel', 'animalAdoptedDate', 'animalAdoptionFee', 'animalAgeString', 'animalAltered', 'animalAvailableDate', 'animalBirthdate', 'animalBirthdateExact', 'animalBreed', 'animalCoatLength', 'animalColor', 'animalColorID', 'animalColorDetails', 'animalCourtesy', 'animalDeclawed', 'animalDescription', 'animalDescriptionPlain', 'animalDistinguishingMarks', 'animalEarType', 'animalEnergyLevel', 'animalExerciseNeeds', 'animalEyeColor', 'animalFence', 'animalFound', 'animalFoundDate', 'animalFoundPostalcode', 'animalGeneralAge', 'animalGeneralSizePotential', 'animalGroomingNeeds', 'animalHousetrained', 'animalIndoorOutdoor', 'animalKillDate', 'animalKillReason', 'animalLocation', 'animalLocationCoordinates', 'animalLocationDistance', 'animalLocationCitystate', 'animalMicrochipped', 'animalMixedBreed', 'animalName', 'animalSpecialneeds', 'animalSpecialneedsDescription', 'animalNeedsFoster', 'animalNewPeople', 'animalNotHousetrainedReason', 'animalObedienceTraining', 'animalOKWithAdults', 'animalOKWithCats', 'animalOKWithDogs', 'animalOKWithKids', 'animalOwnerExperience', 'animalPattern', 'animalPatternID', 'animalAdoptionPending', 'animalPrimaryBreed', 'animalPrimaryBreedID', 'animalRescueID', 'animalSearchString', 'animalSecondaryBreed', 'animalSecondaryBreedID', 'animalSex', 'animalShedding', 'animalSizeCurrent', 'animalSizePotential', 'animalSizeUOM', 'animalSpecies', 'animalSpeciesID', 'animalSponsorable', 'animalSponsors', 'animalSponsorshipDetails', 'animalSponsorshipMinimum', 'animalStatus', 'animalStatusID', 'animalSummary', 'animalTailType', 'animalThumbnailUrl', 'animalUptodate', 'animalUpdatedDate', 'animalUrl', 'animalVocal', 'animalYardRequired', 'animalAffectionate', 'animalApartment', 'animalCratetrained', 'animalDrools', 'animalEagerToPlease', 'animalEscapes', 'animalEventempered', 'animalFetches', 'animalGentle', 'animalGoodInCar', 'animalGoofy', 'animalHasAllergies', 'animalHearingImpaired', 'animalHypoallergenic', 'animalIndependent', 'animalIntelligent', 'animalLap', 'animalLeashtrained', 'animalNeedsCompanionAnimal', 'animalNoCold', 'animalNoFemaleDogs', 'animalNoHeat', 'animalNoLargeDogs', 'animalNoMaleDogs', 'animalNoSmallDogs', 'animalObedient', 'animalOKForSeniors', 'animalOKWithFarmAnimals', 'animalOlderKidsOnly', 'animalOngoingMedical', 'animalPlayful', 'animalPlaysToys', 'animalPredatory', 'animalProtective', 'animalSightImpaired', 'animalSkittish', 'animalSpecialDiet', 'animalSwims', 'animalTimid', 'fosterEmail', 'fosterFirstname', 'fosterLastname', 'fosterName', 'fosterPhoneCell', 'fosterPhoneHome', 'fosterSalutation', 'locationAddress', 'locationCity', 'locationCountry', 'locationUrl', 'locationName', 'locationPhone', 'locationState', 'locationPostalcode', 'animalPictures', 'animalVideos', 'animalVideoUrls'
+                ]
+
+        }
+
+    }
+    return search;
+}
+
+const makeAPISearchRequest = (() => {
+
+    const searchObject = createSearchObject();
+
+    const promise = new Promise((resolve, reject) => {
+        var xhr = new XMLHttpRequest();
+        xhr.open('POST', baseUrl, true);
+        xhr.setRequestHeader('Content-Type', 'application/json');
+        xhr.onload = function () {
+            if (xhr.status === 200) {
+                const json = JSON.parse(xhr.responseText);
+                resolve(json);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        const data = JSON.stringify(searchObject);
+        xhr.send(data);
+    });
+
+    return promise;
+})
 
 // build search url
 
@@ -11,6 +98,16 @@ const submit = document.querySelector('#submit-btn');
 // handle data, break into chunks (pages?)
 
 // display pets returned, in pages? on borderless cards
+const submitSearch = (event) => {
+    event.preventDefault();
+    makeAPISearchRequest()
+    .then(response => displaySearch(response));
+    //console.log(promise, 'this is the promise in the event handler');
+
+    //buildSearchURL();
+}
+
+submit.addEventListener('click', submitSearch)
 
 
 ///// login path //////////
---
+++ readme.txt
@@ -0,0 +1,80 @@
+rescueGroups.org:
+API docs: 
+Your API Key: 6QONihuq
+
+request url: https://api.rescuegroups.org/http/v2.json
+
+https://rescuegroups.org/services/adoptable-pet-data-api/
+
+
+zip url: https://www.zipcodeapi.com/
+
+zip api key: r5gWDlTNjPSGXpsRxxAVstSqVOHZYSuwK8UsK2bV77SL6oaJzADtrfleTjv6D9jK
+
+Petfinder:
+12/27/2019
+
+proxy server, add this before url
+https://corsproxy.github.io
+
+Pet finder api credentials, waiting for reply about domain origin (cors issue)
+api key  kTkq5OF4L9FjO4bQcMXrSSKZZZQiLKJ82ZIqG1JJhhPPt6kD4A
+secret 5fycV7UzMZBF2aoixMVXtIrQ306XSerhfkO29uZA
+
+token request (curl):
+curl -d "grant_type=client_credentials&client_id=kTkq5OF4L9FjO4bQcMXrSSKZZZQiLKJ82ZIqG1JJhhPPt6kD4A&client_secret=5fycV7UzMZBF2aoixMVXtIrQ306XSerhfkO29uZA" https://api.petfinder.com/v2/oauth2/token
+
+launch chrome without cross site security:
+open -a /Applications/Google\ Chrome.app --args --disable-web-security --user-data-dir="/tmp/chrome_tmp"
+
+
+to get breeds list:
+https://dog.ceo/dog-api/documentation/
+
+
+rest calls:
+
+const makeAPIRequest = () => {
+
+/*
+    var request = new Request('https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/token', {
+        method: 'GET',
+        mode: 'cors',
+        headers: new Headers({
+            grant_type: 'client_credentials',
+            client_id: 'kTkq5OF4L9FjO4bQcMXrSSKZZZQiLKJ82ZIqG1JJhhPPt6kD4A',
+            client_secret: '5fycV7UzMZBF2aoixMVXtIrQ306XSerhfkO29uZA'
+        })
+    });
+
+    fetch(request)
+    .then(response => {return response.json()})
+    .then(myJson => console.log(myJson));
+*/
+/*
+    axios({
+        method: 'GET',
+        headers: {grant_type: 'client_credentials',
+            client_id: 'kTkq5OF4L9FjO4bQcMXrSSKZZZQiLKJ82ZIqG1JJhhPPt6kD4A',
+            client_secret: '5fycV7UzMZBF2aoixMVXtIrQ306XSerhfkO29uZA'},
+        url: 'https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/token',
+    }).then(function (response) {
+        console.log(response.data);
+    });
+*/
+}
+/*
+const makeAPIRequest = () => {
+    const options = {
+        headers: {
+            grant_type: 'client_credentials',
+            client_id: 'kTkq5OF4L9FjO4bQcMXrSSKZZZQiLKJ82ZIqG1JJhhPPt6kD4A',
+            client_secret: '5fycV7UzMZBF2aoixMVXtIrQ306XSerhfkO29uZA'
+        }
+    };
+
+    let promise = axios.get('https://cors-anywhere.herokuapp.com/https://api.petfinder.com/v2/oauth2/token',  options)
+        .then(response => console.log(response));
+    console.log(promise);
+}
+*/
